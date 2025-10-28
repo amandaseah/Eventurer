55ea83f9691e0bffaef7d3166aa46e02ffd7efaa
nethren <nethrenbalamurugan@gmail.com>
2025-10-28 17:37:10 +0800

    making vercel work

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,14 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
 import { getFirestore, doc, setDoc, serverTimestamp } from 'firebase/firestore';
 
-const metaEnv: any = (import.meta as any).env || {};
 const firebaseConfig = {
-  apiKey: metaEnv.VITE_FIREBASE_API_KEY,
-  authDomain: metaEnv.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: metaEnv.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: metaEnv.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: metaEnv.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: metaEnv.VITE_FIREBASE_APP_ID,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
--- vite-env.d.ts
+++ vite-env.d.ts
@@ -4,6 +4,8 @@ interface ImportMetaEnv {
     readonly VITE_GOOGLE_MAPS_API_KEY: string;
     // add other env vars here if needed, e.g.
     // readonly VITE_BACKEND_URL: string;
+
+
   }
   
   interface ImportMeta {
