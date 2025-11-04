a05d1c0699a32e68a14c21fd4e2be0ccdf7feda6
nethren <nethrenbalamurugan@gmail.com>
2025-11-04 18:41:01 +0800

    payment.js edit

--- api/create-payment-intent.js
+++ api/create-payment-intent.js
@@ -1,4 +1,5 @@
 require("dotenv").config({path: ".env.local"});
+require("dotenv").config({path: ".env"});
 const express = require("express");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
