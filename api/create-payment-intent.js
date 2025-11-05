50759604376d861663d46790ed34b11b293b44f0
amandaseah <amanda.seah.2024@smu.edu.sg>
2025-11-05 20:12:07 +0800

    fixed alignment issue in event info page

--- api/create-payment-intent.js
+++ api/create-payment-intent.js
@@ -17,10 +17,13 @@ async function handler(req, res) {
       receipt_email,
       metadata = {},
      } = req.body || {};
+
     const paymentIntent = await stripe.paymentIntents.create({
       amount,
       currency,
       description,
+      receipt_email,
+      metadata,
       automatic_payment_methods: { enabled: true },
     });
     res.status(200).json({ clientSecret: paymentIntent.client_secret });
